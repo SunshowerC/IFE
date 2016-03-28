1d4fc460006b56ca3981e0c5f0ddebd7ad339e91
Weiyu-Chen <1300546909@qq.com>
2016-03-28 12:35:54 +0800

    任务十八

--- IFE-Mission2/js/task18.js
+++ IFE-Mission2/js/task18.js
@@ -1,7 +1,34 @@
 
+var btn = document.getElementsByTagName("input");
 
+function trim(str){
+	return str.replace(/\s/g,"");
+}
 
+function add_block() {
+	var wrap = document.getElementsByClassName("block")[0];
+	var block = document.createElement("div");
+	block.innerHTML= trim(btn[0].value);
 
+	if(this.value=="左侧入")
+		wrap.insertBefore(block,wrap.firstChild);
+	else if(this.value == "右侧入")
+		wrap.appendChild(block);
+//		wrap.insertBefore(block,wrap.lastChild);
+	else if(this.value == "左侧出")
+		wrap.removeChild(wrap.firstElementChild);
+	else 
+		wrap.removeChild(wrap.lastElementChild);
+	block.onclick = function(){
+		wrap.removeChild(block);
+	}
+}
 
+function init() {
+	for (var i = 1; i < btn.length; i++) {
+		btn[i].onclick = add_block;
+	}
+}
 
+init();
 
