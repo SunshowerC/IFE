d56671a09a6a9ea19769dbfcd128bc7a7281ac68
Weiyu-Chen <1300546909@qq.com>
2016-03-28 23:01:39 +0800

    任务二十

---
+++ IFE-Mission2/js/task20.js
@@ -0,0 +1,55 @@
+
+var btn = document.getElementsByTagName("input");
+var wrap = document.getElementsByClassName("block")[0];
+
+
+function remove_clicked(obj){
+	obj.onclick = function(){
+		wrap.removeChild(obj);
+	}
+}
+
+
+function init() {
+	for (var i = 0; i < btn.length; i++) {
+		btn[i].onclick = render;
+	}
+}
+
+
+function render() {
+	var text = document.getElementsByTagName("textarea")[0].value;
+	var now_text =	text.split(/[,，;；、\s]+/);
+	
+		switch(this.value){
+			case "左侧入" :
+				if(text==""){ alert("魂淡，啥都没输入啊！");return false; }
+				for (var i = 0; i < now_text.length; i++) {
+					var block = document.createElement("div");		
+					block.innerHTML = now_text[i] ;
+					remove_clicked(block);
+					wrap.insertBefore(block,wrap.firstChild);				
+				}
+				break;
+
+			case "右侧入" :
+				if(text==""){ alert("魂淡，啥都没输入啊！");return false; }
+				for (var i = 0; i < now_text.length; i++) {
+					var block = document.createElement("div");		
+					block.innerHTML = now_text[i] ;
+					remove_clicked(block);
+					wrap.appendChild(block,wrap.firstChild);				
+				}				
+				break;
+			case "左侧出" :
+				wrap.removeChild(wrap.firstElementChild);break;
+			case "右侧出" :
+				wrap.removeChild(wrap.lastElementChild);break;		
+			
+		}		
+	
+}
+
+
+init();
+
