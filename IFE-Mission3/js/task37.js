a2e92b937987fdb6df92645e4c58b728df87ef3d
weiyu <1300546909@qq.com>
2016-04-13 15:18:17 +0800

    任务37 2.0

--- IFE-Mission3/js/task37.js
+++ IFE-Mission3/js/task37.js
@@ -1,59 +1,64 @@
-var btn = document.querySelector('.content button');
+(function(){
+	var btn = document.querySelector('.content button');
 
-function Pop(width,height,title,content) {
-	this.obj = document.createElement('div');
-	this.disX = 0;
-	this.disY = 0;
-	this.obj.id = 'pop';
-	this.obj.style.width = width + 'px' ;
-	this.obj.style.height = height + 'px' ;
-	this.title = title;
-	this.content = content;
-
-}
-
-Pop.prototype.init = function() {
-	var This = this;	
-	
-	//增加遮罩
-	var mask = document.createElement('div');
-	mask.className = 'mask';
-	document.body.appendChild(mask);
-
-	//增加弹出层
-	This.obj.innerHTML = '<h2>' + This.title + '</h2>' + '<p>' + This.content + '</p>';
-	document.body.appendChild(This.obj);
-
-	// 弹出层移动
-	This.obj.onmousedown = function(ev) {
-		var ev = ev || window.event;
-		This.disX = ev.clientX - This.obj.offsetLeft; //获取光标相对位置
-		This.disY = ev.clientY - This.obj.offsetTop;	
-		document.onmousemove = function(ev)	 {
-			var ev = ev || window.event;
-			This.obj.style.left = ev.clientX - This.disX + 'px';
-			This.obj.style.top = ev.clientY - This.disY + 'px';
-		}
-
-		document.onmouseup = function() {	
-			document.onmousedown = null;
-			document.onmousemove = null;
-		}
-			
+	function Pop(width,height,title,content) {
+		this.obj = document.createElement('div');
+		this.disX = 0;
+		this.disY = 0;
+		this.obj.id = 'pop';
+		this.obj.style.width = width + 'px' ;
+		this.obj.style.height = height + 'px' ;
+		this.title = title;
+		this.content = content;
 	}
 
-	This.obj.onclick = function(ev) {
-		var ev = ev || window.event;
-		ev.stopPropagation();
-	}	
-	document.onclick = function() {
-		document.body.removeChild(This.obj);
-		document.body.removeChild(mask);
-		document.onclick =null;
-	}		
-}
+	Pop.prototype = {
+		init: function(){
+			this.createDom();
+			this.bindEvent();
+		},
+		createDom: function(){
+			//增加遮罩
+			this.mask = document.createElement('div');
+			this.mask.className = 'mask';
+			document.body.appendChild(this.mask);
 
-window.onload = function(){
+			//增加弹出层
+			this.obj.innerHTML = '<h2>' + this.title + '</h2>' + '<p>' + this.content + '</p>';
+			document.body.appendChild(this.obj);		
+		},
+		bindEvent: function() {
+			var This = this;
+			// 弹出层移动
+			This.obj.onmousedown = function(ev) {
+				var ev = ev || window.event;
+				This.disX = ev.clientX - This.obj.offsetLeft; //获取光标相对位置
+				This.disY = ev.clientY - This.obj.offsetTop;	
+				document.onmousemove = function(ev)	 {
+					var ev = ev || window.event;
+					This.obj.style.left = ev.clientX - This.disX + 'px';
+					This.obj.style.top = ev.clientY - This.disY + 'px';
+				}
+
+				document.onmouseup = function() {	
+					document.onmousedown = null;
+					document.onmousemove = null;
+				}	
+			}
+
+			//移除遮罩和弹出层
+			This.obj.onclick = function(ev) {
+				var ev = ev || window.event;
+				ev.stopPropagation();
+			}	
+			document.onclick = function() {
+				document.body.removeChild(This.obj);
+				document.body.removeChild(This.mask);
+				document.onclick =null;
+			}		
+		}
+	}
+
 	btn.onclick = function(ev){
 		var ev = ev || window.event;
 		ev.stopPropagation();		
@@ -63,6 +68,7 @@ window.onload = function(){
 			多的内容，内容，好多的内容。');
 		d.init();		
 	}
+}() )
 
-}
+
 
