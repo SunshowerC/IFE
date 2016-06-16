ee82de30c841cf31857519dfa907898f19eede33
weiyu <1300546909@qq.com>
2016-06-16 14:46:52 +0800

    update the tngou photo API

--- IFE-Mission3/js/task44-ajax.js
+++ IFE-Mission3/js/task44-ajax.js
@@ -1,13 +1,8 @@
 
 
 
-var img = {
-	src:'./img/0.jpg',
-	title:'planet',
-	description: '图片描述信息'
-}
 
-
+/*
 function getPhoto(){
 	var request = new XMLHttpRequest();
 	var count = Math.ceil( Math.random() * 5000 );
@@ -22,6 +17,110 @@ function getPhoto(){
 		}
 	}
 }
+*/
+
+/*
+CORS跨域请求，不行？
+function createCORSRequest(method,url) {
+	var xhr = new XMLHttpRequest();
+	if ("withCredentials" in xhr ) {
+		xhr.open(method, url, true);
+	} else if(typeof XDomainRequest != "undefined") {
+		xhr = new XDomainRequest();
+		xhr.open(method, url);
+
+	} else {
+		xhr = null;
+	}
+	return xhr;
+
+}
+
+function getPhoto(){
+
+	var count = Math.ceil( Math.random() * 700 );
+	var url = 'http://www.tngou.net/tnfs/api/show?id='+ (count++);
+	console.log(url)
+	var request = createCORSRequest('GET',url);
+	console.log(request)
+	if (request) {
+		request.onload = function () {
+			console.log(request.responseText);
+		}
+		request.send();
+	}
+
+}
+
+*/
+
+
+//jsonp 获取数据
+function getJSONP(url,callback) {
+	var cbnum = "cb" + getJSONP.counter++;
+	var cbname = "getJSONP." + cbnum;
+
+	if (url.indexOf("?") == -1) {
+		url += "?callback=" + cbname;
+	} else {
+		url += "&callback=" + cbname;
+	}
+
+	var script = document.createElement("script");
+	getJSONP[cbnum] = function (response) {
+		try {
+			callback(response);
+		}
+		finally {
+			delete getJSONP[cbnum];
+			script.parentNode.removeChild(script);
+		}
+	};
+
+	script.src = url;
+	document.body.appendChild(script);
+}
+getJSONP.counter = 0;
+
+
+
+
+var photoAPI = {
+	url: "http://www.tngou.net/tnfs/api/list?",
+	id: 0,
+	page: 1
+}
+
+function addPhoto(galleryWall) {
+
+	getJSONP(photoAPI.url + "id=" + photoAPI.id + "&page=" + photoAPI.page++ ,function (response) {
+		//  var res = JSON.stringify(response)
+		// console.log(response.tngou)
+
+		var list = response.tngou ;
+		if (list.length == 0) {
+			photoAPI.id++;
+			photoAPI.page = 1;
+			return;
+		}
+		list.forEach(function (item) {
+			galleryWall.appendPhoto({
+				src: "http://tnfs.tngou.net/image" + item.img,
+				title: item.title ,
+				desciption: ''
+			});
+		});
+	});
+}
+
+
+
+
+
+
+
+
+/*
 
 function photoNumFormat(num) {
 	if (num < 10) {
@@ -35,3 +134,4 @@ function photoNumFormat(num) {
 	}
 
 }
+*/
--- IFE-Mission3/js/task44.js
+++ IFE-Mission3/js/task44.js
@@ -1,139 +1,118 @@
 
+(function () {
 
-
-function GalleryWall(options){
-	this.elem = document.querySelector(options.elem);
-	this.col =options.col || 5;
-	this.gap = options.gap / 2 || 10 ;
-	this.init();
-}
-
-GalleryWall.prototype.init = function() {
-/*	var galleryWrap = document.createElement('div');
-	galleryWrap.setAttribute('class','gallery');
-	var fragmentHtml  = '';
-	for(var i = 0 ; i < this.col ; i++) {
-		fragmentHtml += '<div class = "gallery-col"' +
-						'style = "width:' + 100/this.col +'%; padding: 0 ' + this.gap + 'px;" >' +
-						'</div>';
+	function GalleryWall(options){
+		this.elem = document.querySelector(options.elem);
+		this.col =options.col || 5;
+		this.gap = options.gap / 2 || 10 ;
+		this.init();
 	}
-	galleryWrap.innerHTML = fragmentHtml;
-	this.elem.appendChild(galleryWrap);
-	this.columns = galleryWrap.querySelectorAll('.gallery-col');*/
 
-	this.createMainDom();
-	this.popUp();
-}
+	GalleryWall.prototype.init = function() {
+		/*	var galleryWrap = document.createElement('div');
+		 galleryWrap.setAttribute('class','gallery');
+		 var fragmentHtml  = '';
+		 for(var i = 0 ; i < this.col ; i++) {
+		 fragmentHtml += '<div class = "gallery-col"' +
+		 'style = "width:' + 100/this.col +'%; padding: 0 ' + this.gap + 'px;" >' +
+		 '</div>';
+		 }
+		 galleryWrap.innerHTML = fragmentHtml;
+		 this.elem.appendChild(galleryWrap);
+		 this.columns = galleryWrap.querySelectorAll('.gallery-col');*/
 
-GalleryWall.prototype.createMainDom = function() {
-	var galleryWrap = document.createElement('div');
-	galleryWrap.setAttribute('class','gallery');
-	var fragmentHtml  = '';
-	for(var i = 0 ; i < this.col ; i++) {
-		fragmentHtml += '<div class = "gallery-col"' +
-						'style = "width:' + 100/this.col +'%; padding: 0 ' + this.gap + 'px;" >' +
-						'</div>';
+		this.createMainDom();
+		this.popUp();
 	}
-	galleryWrap.innerHTML = fragmentHtml;
-	this.elem.appendChild(galleryWrap);
-	this.columns = galleryWrap.querySelectorAll('.gallery-col');
 
-	var mask = document.createElement('div');
-	mask.setAttribute('class','mask');
-	this.mask = mask;
-	document.body.appendChild(mask);
-}
-
-GalleryWall.prototype.appendPhoto = function(photo) {
-	var newPhoto = document.createElement('div');
-	newPhoto.className = 'gallery-item';
-	newPhoto.setAttribute('style','margin-bottom:' + this.gap * 2 + 'px');
-	newPhoto.innerHTML = 
-	'<div class="gallery-image">' +
-		'<img src=' + photo.src + '>' +
-	'</div>' +
-	'<div class="gallery-item-info">' +
-		'<h4 class="gallery-title">' + photo.src.substr(30,5) + '</h4>' +
-		'<p class="gallery-description">' + photo.description + '</p>' +
-	'</div>' ;
-	this.getMinHeight().appendChild(newPhoto);
-}
-
-GalleryWall.prototype.getMinHeight = function() {
-	var minColumn = this.columns[0];
-	for (var i = 1, len = this.columns.length; i < len; i++) {
-		if (this.columns[i].clientHeight < minColumn.clientHeight) {
-		  minColumn = this.columns[i];
+	GalleryWall.prototype.createMainDom = function() {
+		var galleryWrap = document.createElement('div');
+		galleryWrap.setAttribute('class','gallery');
+		var fragmentHtml  = '';
+		for(var i = 0 ; i < this.col ; i++) {
+			fragmentHtml += '<div class = "gallery-col"' +
+				'style = "width:' + 100/this.col +'%; padding: 0 ' + this.gap + 'px;" >' +
+				'</div>';
 		}
+		galleryWrap.innerHTML = fragmentHtml;
+		this.elem.appendChild(galleryWrap);
+		this.columns = galleryWrap.querySelectorAll('.gallery-col');
+
+		var mask = document.createElement('div');
+		mask.setAttribute('class','mask');
+		this.mask = mask;
+		document.body.appendChild(mask);
 	}
 
-	// console.log(min);
-	return minColumn;
-}
+	GalleryWall.prototype.appendPhoto = function(photo) {
+		var newPhoto = document.createElement('div');
+		newPhoto.className = 'gallery-item';
+		newPhoto.setAttribute('style','margin-bottom:' + this.gap * 2 + 'px');
+		newPhoto.innerHTML =
+			'<div class="gallery-image">' +
+			'<img src=' + photo.src + '>' +
+			'</div>' +
+			'<div class="gallery-item-info">' +
+			'<h4 class="gallery-title">' + photo.title + '</h4>' +
+			'<p class="gallery-description">' + photo.description + '</p>' +
+			'</div>' ;
+		this.getMinHeight().appendChild(newPhoto);
+	}
 
-GalleryWall.prototype.popUp = function() {
-	var This = this;
-	This.elem.onclick = function(e) {
-		var target = e.target;
-		if (target.tagName.toLowerCase() == 'img' ) {
-			This.mask.style.display = 'block';
-			This.mask.innerHTML = '';
-			var img = document.createElement('img');
-			img.setAttribute('src',target.src );
-			This.mask.appendChild(img);
-			setSize(img);
-			document.body.style.overflowY = 'hidden';
+	GalleryWall.prototype.getMinHeight = function() {
+		var minColumn = this.columns[0];
+		for (var i = 1, len = this.columns.length; i < len; i++) {
+			if (this.columns[i].clientHeight < minColumn.clientHeight) {
+				minColumn = this.columns[i];
+			}
 		}
 
-		e.stopPropagation()	;
-		document.onclick = function(e) {
-			This.mask.style.display = 'none';
-			document.body.style.overflowY = '';
-			document.onclick = null;
-		}		
+		// console.log(min);
+		return minColumn;
 	}
 
-	function setSize(target) {
-		var width = target.offsetWidth;
+	GalleryWall.prototype.popUp = function() {
+		var This = this;
+		This.elem.onclick = function(e) {
+			var target = e.target;
+			if (target.tagName.toLowerCase() == 'img' ) {
+				This.mask.style.display = 'block';
+				This.mask.innerHTML = '';
+				var img = document.createElement('img');
+				img.setAttribute('src',target.src );
+				This.mask.appendChild(img);
+				setSize(img);
+				document.body.style.overflowY = 'hidden';
+			}
+
+			e.stopPropagation()	;
+			document.onclick = function(e) {
+				This.mask.style.display = 'none';
+				document.body.style.overflowY = '';
+				document.onclick = null;
+			}
+		}
+
+		function setSize(target) {
+			var width = target.offsetWidth;
 			height = target.offsetHeight;
-	    var imageAspectRatio = width / height;
-	    var windowAspectRatio = innerWidth / innerHeight;
-	    // console.log(height + ',' + innerHeight);
-	    // console.log(imageAspectRatio+ '<'+ windowAspectRatio)
-		if (height > innerHeight && imageAspectRatio < windowAspectRatio) {
-			target.style.height = innerHeight - 100 + 'px';
+			var imageAspectRatio = width / height;
+			var windowAspectRatio = innerWidth / innerHeight;
+			// console.log(height + ',' + innerHeight);
+			// console.log(imageAspectRatio+ '<'+ windowAspectRatio)
+			if (height > innerHeight && imageAspectRatio < windowAspectRatio) {
+				target.style.height = innerHeight - 100 + 'px';
+			}
+
+			if (width > innerWidth && imageAspectRatio > windowAspectRatio) {
+				target.style.width = innerWidth - 100 + 'px';
+			}
 		}
-
-		if (width > innerWidth && imageAspectRatio > windowAspectRatio) {
-			target.style.width = innerWidth - 100 + 'px';
-		}
 	}
-}
+	
+	window.GalleryWall = GalleryWall;
 
-
-var newGallerywall = new GalleryWall({
-	elem : '.container',
-	col: 4,
-	gap: '30'
-
-});
-
-
-window.onload = function() {
-	for (var i = 0; i < 15 ; i++) {
-		getPhoto();
-	}
-}
-
-var offsetDistance = 100;
-
-window.onscroll = function() {
-	var screenHeight = (document.documentElement.scrollTop || document.body.scrollTop) +(document.documentElement.clientHeight || document.body.clientHeight);
-	var docHeight = document.body.scrollHeight;
-	if (docHeight < screenHeight + offsetDistance ) {
-		getPhoto();
-	}
-}
+})()
 
 
 
